66218461fe6da0728be10aaaae2b9747f0d635d8
Sujal-Agrwal <sujal2919@gmail.comgit>
2021-02-16 23:51:44 +0530

    My_Profile

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Sujal Agrawal",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Sujal Agrawal and I'm a Software Engineer!",
+  githubUrl: "https://github.com/Sujal-Agrawal",
+  linkedinUrl: "https://www.linkedin.com/in/sujal-agrawal-7345221a5/",
+  cvLink: "https://drive.google.com/file/d/1Ye5LKiLe6sj9K9KFlDCPkxwpQOb7nNfN/view?usp=sharing",
+};
